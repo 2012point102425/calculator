1677c02b5faebe93347b75326f9678de49391b81
Korbin Hiebert <2012.102425@plpsd.net>
2024-01-09 14:22:09 -0600

    Currently trying to get the State.build error to go away, unsure on how to right now

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,111 +1,203 @@
 import 'package:flutter/material.dart';
-
-class CalculatorHomePage extends StatefulWidget {
-  const CalculatorHomePage({super.key, this.title=''});
-  final String title;
+import 'package:calculator_app/buttons.dart';
+class CalculatorView extends StatefulWidget {
+  const CalculatorView({super.key});
 
   @override
-  State<CalculatorHomePage> createState() => _CalculatorHomePageState();
+  State<CalculatorView> createState() => _CalculatorViewState();
 }
 
-class _CalculatorHomePageState extends State<CalculatorHomePage> {
-  final String _str='o';
-  add(String a)  {
+class _CalculatorViewState extends State<CalculatorView> {
+  
+  String equation = "0";
+  String result = "0";
+  String expression = "";
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
 
+  buttonPressed(String buttonText) {
+// used to check if the result contains a decimal
+    String doesContainDecimal(dynamic result) {
+      if (result.toString().contains('.')) {
+        List<String> splitDecimal = result.toString().split('.');
+        if (!(int.parse(splitDecimal[1]) > 0)) {
+          return result = splitDecimal[0].toString();
+        }
+      }
+      return result;
+    }
+
+    setState(() {
+      if (buttonText == "AC") {
+        equation = "0";
+        result = "0";
+      } else if (buttonText == "⌫") {
+        equation = equation.substring(0, equation.length - 1);
+        if (equation == "") {
+          equation = "0";
+        }
+      } else if (buttonText == "+/-") {
+        if (equation[0] != '-') {
+          equation = '-$equation';
+        } else {
+          equation = equation.substring(1);
+        }
+      } else if (buttonText == "=") {
+        expression = equation;
+        expression = expression.replaceAll('×', '*');
+        expression = expression.replaceAll('÷', '/');
+        expression = expression.replaceAll('%', '%');
+
+        
   }
-deleteAll() {
-return "";
-}
-deleteOne() {
-
-}
-getResult() {
-
-}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    backgroundColor: Colors.black54,
     appBar: AppBar(
-    title: const Text('something'),
-    ),
-  
-  
-    body: Column(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-Card(
-color: Colors.lightGreen[50],
-child: Padding(
-padding: const EdgeInsets.all(15.0),
-child: Text(
-_str,
-textScaleFactor: 2.0,
-),
-),
-),
-Row(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-  ElevatedButton(
-
-style: const TextStyle(color: Colors.white),
-
-onPressed: (){deleteAll();},
-
-child: const Text(
-'C',
-),
+    elevation: 0,
+    backgroundColor: Colors.black54,
+    leading: const Icon(Icons.settings, color: Colors.orange),
+    actions: const [
+  Padding(
+    padding: EdgeInsets.only(top: 18.0),
+    child: Text('DEG', style: TextStyle(color: Colors.white38)),
   ),
-ElevatedButton(
-child: const Text(
-'<-',
-style: TextStyle(color: Colors.white)
-),
-onPressed: (){deleteOne();},
+  SizedBox(width: 20),
+  ],
+  ),
+    body: SafeArea(
+    child: Column(mainAxisAlignment: MainAxisAlignment.end,
+    children: [
+  Align(
+    alignment: Alignment.bottomRight,
+    child: SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+  Row(
+    mainAxisAlignment: MainAxisAlignment.end,
+    children: <Widget>[
+  Padding(
+    padding: const EdgeInsets.all(10.0),
+    child: Text(result,
+    textAlign: TextAlign.left,
+    style: const TextStyle(
+    color: Colors.white, fontSize:80))),
+    const Icon(Icons.more_vert,
+    color: Colors.orange, size: 30),
+    const SizedBox(width: 20),
+  ],
+  ),
+  Row(
+    mainAxisAlignment: MainAxisAlignment.end,
+    children: [
+  Padding(
+    padding: const EdgeInsets.all(20),
+    child: Text(equation,
+    style: const TextStyle(
+    fontSize: 40,
+    color: Colors.white38,
+  )
+  ),
+  ),
+  IconButton(
+    icon: const Icon(Icons.backspace_outlined,
+    color: Colors.orange, size: 30),
+    onPressed: () {
+    buttonPressed("⌫");
+  },
+  ),
+    const SizedBox(width: 20),
+  ],
+  )
+  ],
+  ),
+  ),
+  ),
+  Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+  calcButton('AC', Colors.white10, () => buttonPressed('AC')),
+  calcButton('%', Colors.white10, () => buttonPressed('%')),
+  calcButton('÷', Colors.white10, () => buttonPressed('÷')),
+  calcButton("×", Colors.white10, () => buttonPressed('×')),
+  ],
+  ),
+    const SizedBox(height: 10),
 
+  Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+  calcButton('7', Colors.white24, () => buttonPressed('7')),
+  calcButton('8', Colors.white24, () => buttonPressed('8')),
+  calcButton('9', Colors.white24, () => buttonPressed('9')),
+  calcButton('-', Colors.white10, () => buttonPressed('-')),
+  ],
+  ),
+    const SizedBox(height: 10),
 
-),
-],
-),
-const Row(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-Column(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-Row(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-],
+  Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+  calcButton('4', Colors.white24, () => buttonPressed('4')),
+  calcButton('5', Colors.white24, () => buttonPressed('5')),
+  calcButton('6', Colors.white24, () => buttonPressed('6')),
+  calcButton('+', Colors.white10, () => buttonPressed('+')),
+  ],
+  ),
+    const SizedBox(height: 10),
+    // calculator number buttons
 
-),
-Row(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children:<Widget>[
-],
-),
-Row(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-],
-),
-Row(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-],
-),
-],
-),
-Column(
-crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-],
-),
-],
-),
-],
-),
-    );
-    
+  Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+  Column(
+  //mainAxisAlignment: MainAxisAlignment.spaceAround
+    children: [
+  Row(
+    children: [
+  calcButton(
+    '1', Colors.white24, () => buttonPressed('1')),
+  SizedBox(
+    width: MediaQuery.of(context).size.width * 0.04),
+  calcButton(
+    '2', Colors.white24, () => buttonPressed('2')),
+  SizedBox(
+    width: MediaQuery.of(context).size.width * 0.04),
+  calcButton(
+    '3', Colors.white24, () => buttonPressed('3')),
+  ],
+  ),
+    const SizedBox(height: 10),
+  Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+  calcButton('+/-', Colors.white24,
+    () => buttonPressed('+/-')),
+  SizedBox(
+    width: MediaQuery.of(context).size.width * 0.04),
+  calcButton(
+    '0', Colors.white24, () => buttonPressed('0')),
+  SizedBox(
+    width: MediaQuery.of(context).size.width * 0.04),
+  calcButton(
+    '.', Colors.white24, () => buttonPressed('.')),
+  ],
+  ),
+  ],
+  ),
+  calcButton('=', Colors.orange, () =>                         buttonPressed('=')),
+  ],
+  )
+
+  ],
+  )
+  )
+  );
   }
-}
+  }
+  );
+  }
+  }
